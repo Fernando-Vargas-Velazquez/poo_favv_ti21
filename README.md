ba29bb26d1dfd078f73c42ebbeedafe50154b0ad
Fernando Antonio Vargas Velázquez <122810166+Fernando-Vargas-Velazquez@users.noreply.github.com>
2023-01-24 20:26:11 +0000

    CREAMOS el programa3.py (operaciones aritméticas)

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 |1 |programa0.py|Hola mundo en python|
 |2 |programa1.py|Comentarios multiples, comentarios de una linea y concatenación y cadena de caracteres|
 |3 |programa2.py|Formatear las cadenas (format), logrando asi imprimirlas sin importar que tipo de dato sea|
+|4 |programa3.py|Format y operaciones aridméticas|
---
+++ semana2/programa3.py
@@ -0,0 +1,18 @@
+"""
+    #Programa 3
+    #Nombre: Fernando AVV
+    #Fecha: 24/01/2023
+    #Descripción: Format y operaciones aridméticas
+"""
+numero1 = 10
+numero2 = 5
+
+print(numero1 + numero2)#Este print imprimira el número 15
+
+print("{}+{}". format(numero1, numero2))#Este print imprimira 10+5
+
+print("{}+{}=". format(numero1, numero2) , (numero1+numero2))#Este print imprimira 10+5=15
+
+print((numero1*numero2),"=", "{}*{}". format(numero2, numero1) )#Este print imprimira 50 = 2*10
+
+print("El resultado de dividir:","{}/{}". format(numero1, numero2) ,"es", (numero1/numero2))#Este print imprimira "El resultado de dividir: 10/5 es 2"
