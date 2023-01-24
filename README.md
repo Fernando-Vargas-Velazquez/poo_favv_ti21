29e54b92827899e8b97c8212938bc95efd0411b2
Fernando Antonio Vargas Velázquez <122810166+Fernando-Vargas-Velazquez@users.noreply.github.com>
2023-01-24 19:58:18 +0000

    CREAMOS el programa2.py usando (format)

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 |--|------------|--------------------|
 |1 |programa0.py|Hola mundo en python|
 |2 |programa1.py|Comentarios multiples, comentarios de una linea y concatenación y cadena de caracteres|
+|3 |programa2.py|Formatear las cadenas (format), logrando asi imprimirlas sin importar que tipo de dato sea|
---
+++ semana2/programa2.py
@@ -0,0 +1,15 @@
+"""
+    #Programa 2
+    #Nombre: Fernando AVV
+    #Fecha: 24/01/2023
+    #Descripción: Formatear las cadenas (format), logrando asi imprimirlas sin importar que tipo de dato sea
+"""
+
+variable1 = "Hola " #variable de tipo str
+variable2 = 619 #variable de tipo int
+print("{}{}". format(variable1, variable2)) #Al utilizar un espacio entre las llaves este va a dar error
+#Las llaves se agregan segun las variables que tengamos, no se pueden utilizar más
+
+"""
+ El (format) nos permite formatear valores y no importa del tipo de dato que se este utilizando :D
+"""
