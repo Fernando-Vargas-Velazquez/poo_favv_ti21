c582928cb299fea945dc3a0dd09ec848800379d3
Fernando Antonio Vargas Velázquez <122810166+Fernando-Vargas-Velazquez@users.noreply.github.com>
2023-01-30 16:55:47 +0000

    CREACIÓN del programa5.py que realiza operaciones aritméticas

--- semana3/programa5.py
+++ semana3/programa5.py
@@ -1,11 +1,11 @@
 """
-    #Programa 4
+    #Programa 5
     #Nombre: Fernando AVV
     #Fecha: 26/01/2023
     #Descripción: Se pide el valor de una variable a través de un (input) y hace diferentes operaciones
 """
-numero1 = int(input("Numero 1: ")) #Ingresa una variable  de nombre numero1
-numero2 = 10 #Asigna una variable con valor 10
+numero1 = int(input("Número 1: ")) #Ingresa una variable  de nombre numero1
+numero2 = int(input("Número 2:")) #Asigna una variable con valor 10
 
 suma = numero1 + numero2 #va a sumar las dos variables
 print(suma) # imprime la suma
@@ -17,5 +17,3 @@ div = numero1 / numero2 #va a dividir las dos variables
 print(div) # imprime la división
 potencia = numero1 ** numero2 #va a hacer la potencia con las dos variables
 print(potencia) #imprime la ponencia
-
-
