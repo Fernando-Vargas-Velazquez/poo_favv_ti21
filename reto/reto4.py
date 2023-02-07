6d55ca06cc51b79ffe0779be7ade5ebedf18c1dc
Fernando Antonio Vargas Velázquez <122810166+Fernando-Vargas-Velazquez@users.noreply.github.com>
2023-02-07 02:57:52 +0000

    CREACIÓN del reto4.py que convierte un valor dado en grados Fahrenheit a grados Celsius

---
+++ reto/reto4.py
@@ -0,0 +1,10 @@
+"""
+    #RETO 4
+    #Nombre: Fernando AVV
+    #Fecha: 05/02/2023
+    #Descripción: convierte un valor dado en grados Fahrenheit a grados Celsius
+"""
+fahre = input("Ingresa grados Fahrenheit: ")
+celcius = (int(fahre) - 32) * 5/9
+
+print(fahre, " Son ", celcius,"° Celcius")
