fa879b8cdb41b561575108b213772b885ee0a2d5
Fernando Antonio Vargas Velázquez <122810166+Fernando-Vargas-Velazquez@users.noreply.github.com>
2023-02-12 02:13:27 +0000

    CREACIÓN del reto7.py que calcula el área y perímetro de un cuadrado

--- reto/reto7.py
+++ reto/reto7.py
@@ -1,7 +1,7 @@
 """
     #Soluciones
     #Nombre: Fernando AVV
-    #Fecha: 10/02/2023
+    #Fecha: 11/02/2023
     #Descripción: Calcula el área y perímetro de un cuadrado
 """
 base=float(input("Dime la base:"))
@@ -9,4 +9,3 @@ altura=float(input("Dime la altura:"))
 perimetro = 2*base + 2*altura
 area = base * altura
 print ("Resultados: Área=", area, " Perímetro=",perimetro)
-
